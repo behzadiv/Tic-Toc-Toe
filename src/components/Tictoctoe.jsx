639c75c37b06d4271320d02616975ee9ea378614
Behzad <saeedibehzad@gmail.com>
2022-04-05 16:24:01 +0430

    create structure

---
+++ src/components/Tictoctoe.jsx
@@ -0,0 +1,22 @@
+import "../App.css";
+import { useState } from "react";
+
+const Toictoctoe = () => {
+  return (
+    <div className="App">
+      <section className="container">
+        <div className="section" id="1"></div>
+        <div className="section" id="2"></div>
+        <div className="section" id="3"></div>
+        <div className="section" id="4"></div>
+        <div className="section" id="5"></div>
+        <div className="section" id="6"></div>
+        <div className="section" id="7"></div>
+        <div className="section" id="8"></div>
+        <div className="section" id="9"></div>
+      </section>
+    </div>
+  );
+};
+
+export default Toictoctoe;
