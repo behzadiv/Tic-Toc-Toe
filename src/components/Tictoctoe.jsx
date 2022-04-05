560d682f267a1612e81ab468b8fbb0b5aee931c0
Behzad <saeedibehzad@gmail.com>
2022-04-05 16:33:33 +0430

    define onClick event on selected box by player x or y

--- src/components/Tictoctoe.jsx
+++ src/components/Tictoctoe.jsx
@@ -2,9 +2,29 @@ import "../App.css";
 import { useState } from "react";
 
 const Toictoctoe = () => {
+
+  const [player, setPlayer] = useState("x");
+
+  const [selectedPlayerX, setSelectedPlayerX] = useState([]);
+  const [selectedPlayerY, setSelectedPlayerY] = useState([]);
+  
+  //select number of box and push it in arrayX or arrayY
+  const selectedByPlayers = (e) => {
+    let selectedId = parseInt(e.target.id);
+    let selectedBox =document.getElementById(selectedId)
+    if (player === "x" && selectedBox.innerHTML==="") {
+      setPlayer("y");
+      selectedPlayerX.push(selectedId);
+      selectedBox.innerHTML= "*";
+    } else if(player === "y" && selectedBox.innerHTML===""){
+      setPlayer("x");
+      selectedPlayerY.push(selectedId);
+      selectedBox.innerHTML= "+";
+    }
+  };
   return (
     <div className="App">
-      <section className="container">
+      <section className="container" onClick={(e) => selectedByPlayers(e)}>
         <div className="section" id="1"></div>
         <div className="section" id="2"></div>
         <div className="section" id="3"></div>
